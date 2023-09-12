5d5c87e3e2fd7a87077d41bdd46ab8bfdba060e9
Emery <emery.dittmer@gmail.com>
2023-09-12 15:27:44 -0400

    change to exprt config for html export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    // Add this line to enable static HTML export
+    target: 'experimental-serverless-trace',
+  };
