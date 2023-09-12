fbe40c8d59db97eb223e66bcdc28074c954e1370
Emery <emery.dittmer@gmail.com>
2023-09-12 15:38:21 -0400

    export update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = {
-    // Add this line to enable static HTML export
-    target: 'experimental-serverless-trace',
-  };
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
