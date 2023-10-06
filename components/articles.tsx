98ad3ccd117a0f59e78a112f1fa529ca2e0ece2e
Emery <emery.dittmer@gmail.com>
2023-10-06 01:24:11 -0400

    fix 9

--- components/articles.tsx
+++ components/articles.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+/* import { Carousel } from 'react-responsive-carousel';
 
   
 export default class NextJsCarousel extends Component {
@@ -16,7 +16,7 @@ export default class NextJsCarousel extends Component {
                       <p className="legend"> COVID on Retail</p>
                       </a>
                   </div>
-                  
+
                   <div>
                       <img src="https://media.licdn.com/dms/image/C4D12AQH9DUdk-WYddQ/article-cover_image-shrink_600_2000/0/1647195212134?e=1701907200&v=beta&t=WCIY9SucQw1mqsAwlGuSc50jxGzXhCaWkF2ydUjNLpw" alt="image2" />
                       <a href='https://www.linkedin.com/pulse/note-physical-location-workers-effect-retention-emery-dittmer/'>
@@ -29,4 +29,4 @@ export default class NextJsCarousel extends Component {
             </div>
         );
     }
-};
+}; */
